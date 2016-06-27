8a6cd8df2cdd50fb72f60e7ba4cc1b42de9d1ca3
M. Winfrey <may.tt@protonmail.ch>
2016-06-27 15:04:49 +0100

    Update cricket-counter.asm

--- cricket-counter.asm
+++ cricket-counter.asm
@@ -1,44 +1,98 @@
-    MOVW    0XFF        ; MAKE EIGHT INPUTS
-    MOVWR   TRISA       ; PORTB MAKE INPUTS
+MOVW    0X03    ; MAKE EIGHT INPUTS
+MOVWR   TRISA   ; PORTB MAKE INPUTS
 
-; ==== ADDING ====;
-UP:
+CLEAR:        ;CLEARS THE DISPLAY,THEN SETS TO 0
+   
+CALL COUNT0
+MOVW 0X00
+MOVWR 0XDF
+
+START:
+MOVRW  PORTA
+ANDW   0X01
+JPZ    CHECK_BUTTON_PRESS
+MOVRW  PORTA
+ANDW   0X02
+JPZ    CHECK_BUTTON_PRESS
+JMP    START
+
+CHECK_BUTTON_PRESS:
+MOVRW PORTA
+ORW 0X03
+JPZ CHECK_BUTTON_PRESS
+
+
+DOUBLE_CHECK:
+MOVRW PORTA
+XORW  0X00
+JPZ START
+
+BUTTON_IS_PRESSED:
+INC  0XDE
+
+
+UP:            ;CHECKS IF IT NEEDS TO COUNT UP
     MOVRW  PORTA   
-    ANDW   0X02         ; WHEN B2 PRESSED
-    JPZ    START        ; IF 0, READ AGAIN
-    JMP    ADD_ONE      ;IF 1, THEN ADD 1 TO W
+    ANDW   0X02      ; WHEN B2 PRESSED
+    JPZ    DOWN	 ; IF 0, READ AGAIN
+    JMP    ADD_ONE   ; IF 1, THEN ADD 1 TO W
 
 ADD_ONE:  
-    ADDW   0X01
-    MOVWR  0XF0
-RET
+INC  0XDF
+    JMP READ
     
-;==== SUBBING ====;
-DOWN:
+DOWN:  ;CHECKS IF IT NEEDS TO COUNT DOWN
     MOVRW PORTA
     ANDW  0X01
     JPZ   START
-    JMP   SUB_ONE
+   
+CHECK_0:
+MOVRW  0XDF
+ORW    0X00
+JPZ    START
 
 SUB_ONE:
-    SUBW   0X01
-    MOVWR  0XF0
+DEC 0XDF
+    JMP READ
     
-;==== 0 TO 6 COUNTER ====; 
-START:        
-    CLEAR:                  ;CLEARS THE DISPLAY
-        MOVW 0X00
-        MOVWR PORTB
+; ====== ALARM =======
+READER: 
+    MOVRW  	PORTA    	; MOVE PORTA TO W
+    ANDW   0X06  ; CHECK IF NUM = 6 
+    JPZ      READ       ; GO BACK TO NUM POLLING
+    JMP    	ALARM  	       ; IF 6, THEN JUMP ALARM
 
+ALARM:
+    MOVW 	0x9   	; spin2win
+    MOVWR   PORTC        ; MOVE TO PORTC
+    CALL   	ALARMTIME 	; TIME DELAY SUBROUTINE
+
+ALARMOFF:
+    MOVW	0X03   	; CLOSE BIT0 AND BIT3 
+    MOVWR 	PORTC	    ; SEND TO PORTC
+    JMP    CLEAR      ; START READING AGAIN
+		
+; ===== ALARMON TIME =======
+ALARMTIME:
+    MOVW     0x30   		; 10 SEC
+
+REP1:
+    SUBW     0x1   		; MINUS 1
+    JPZ      FIN    		; I.E. TURN OFF
+    JMP      REP1  		; KEEP -1'ING 
+
+FIN:
+    JMP      ALARMOFF
+
+; ====== LEWIS CODE =======
+; ==== 0 TO 6 COUNTER =====
 
 READ:         ;TRANSLATES THE WORKING REGISTER 
               ;INTO THE 7 SEG DISPLAY
-MOVWR 0XF1    ;SENDS THE WORKING REG TO A MEMORY LOCATION
+NOP
 CON_READ:
-CALL STORED   ;EXTRACTS FROM THE MEMORY LOCATION 
-JPZ  COUNT0
 
-CALL STORED
+CALL STORED ;EXTRACTS FROM THE MEMORY LOCATION
 SUBW 0X01
 JPZ  COUNT1
 
@@ -66,79 +120,57 @@ JPZ  COUNT6
 COUNT0:    
     MOVW    0X7D
     MOVWR   PORTB
-JMP START
+   CALL STOP_COUNT
+RET
 
 COUNT1:
     MOVW    0X05    ; CONTROL DATA TO W (ONE)
     MOVWR   PORTB   ; COPY DATA TO PORT
+    CALL STOP_COUNT
 JMP START  
  
 COUNT2:         
     MOVW    0X5B    ; CONTROL DATA TO W (TWO)
     MOVWR   PORTB   ; COPY DATA TO PORT
+    CALL STOP_COUNT
 JMP START 
 
 COUNT3:
     MOVW    0X4F
     MOVWR   PORTB
+    CALL STOP_COUNT
 JMP START 
 
 COUNT4:
     MOVW    0X27
     MOVWR   PORTB
+    CALL STOP_COUNT
 JMP START 
 
 COUNT5:
     MOVW    0X6E
     MOVWR   PORTB
+    CALL STOP_COUNT
 JMP START 
 
 COUNT6:
     MOVW    0X7E
     MOVWR   PORTB
+    CALL STOP_COUNT
 JMP READER 
 
 STORED:
-MOVRW  0XF1
+MOVRW  0XDF
 RET
 
-; ===== ALARM =======
-READER: 
-    CALL     SMALLDELAY        ; WAIT A BIT
-    MOVRW      PORTA                  ; MOVE PORTA TO W
-    ANDW        0X06                ; CHECK IF NUM = 6 
-    JPZ      READ                ; GO BACK TO NUM POLLING
-    JMP        ALARM             ; IF 6, THEN JUMP ALARM
-
-ALARM:
-    MOVW     0x9                 ; spin2win
-    MOVWR       PORTC           ; MOVE TO PORTC
-    CALL       ALARMTIME         ; TIME DELAY SUBROUTINE
-
-ALARMOFF:
-    MOVW    0X03              ; CLOSE BIT0 AND BIT3 
-    MOVWR     PORTC                ; SEND TO PORTC
-    JMP         READ               ; START READING AGAIN
-        
-; ===== ALARMON-TIME =======
-ALARMTIME:
-    MOVW     0x10           ; 10 SEC
-
-REP1:
-    SUBW     0x1               ; MINUS 1
-    JPZ      FIN            ; I.E. TURN OFF
-    JMP      REP1           ; KEEP -1'ING 
-
-FIN:
-    JMP      ALARMOFF
-    
-; ===== SMALL DELAY =======
-SMALLDELAY:
-        MOVW    0x05           ; 5 SEC
-REP2:
-        SUBW    0x1            ; MINUS 1
-        JPZ     DONE           ; GOTO READER
-        JMP     REP2           ; KEEP -1'ING
-DONE:
-        JMP     READER         ; READ AGAIN
-
+STOP_COUNT:
+     DEC     0XDE
+     MOVRW   0XDE
+     JPZ     START
+LET_IT_GO:
+     MOVRW   PORTA
+     ORW     0X00
+     JPZ     SWITCHED
+     JMP     LET_IT_GO
+SWITCHED:     
+RET

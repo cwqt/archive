a7e68f3e817a57ac159066d61d0b1d49f7d8cf04
twentytwoo <may.tt@protonmail.ch>
2017-01-12 20:52:04 +0000

    modified:   cw-stepper-v2edit.asm

--- cw-stepper-v2edit.asm
+++ cw-stepper-v2edit.asm
@@ -44,46 +44,55 @@ SORE:
 ; ==== NOTE LENGTH ==== ;
 FOUR: ; 1.579 sec.
     MOVW    FOURd       ; put four delay length into W for TMR
-    JMP     MASTERDELAY ; start up TMR delay
+    JMP     INITDELAY   ; move value into W, to then be stored in N_LENGTH
 
 EIGHT: ; 0.789 sec.
     MOVW    EIGHTd      ; put eight delay length into W for TMR
-    JMP     MASTERDELAY ; start up TMR delay
+    JMP     INITDELAY   ; move value into W, to then be stored in N_LENGTH
 
 SIXTEEN: ; 0.395 sec.
     MOVW    SIXTEENd    ; put sixteen delay length into W for TMR
-    JMP     MASTERDELAY ; start up TMR delay
+    JMP     INITDELAY   ; move value into W, to then be stored in N_LENGTH
 ;=======================;
 
 ; == TIMING FUNCTION == ;
-MASTERDELAY:
-   MOVWR N_LENGTH       ; store n loop amount, n * 10ms = delay (s)
-   
+INITDELAY:
+   MOVWR    N_LENGTH    ; store n loop amount in a mem. location,
+                        ; i.e. iterate FOUR/EIGHT/SIX.d times
+                        ; therefore delay = Fd/Ed/Sd * 10ms
+
 DELAY:
-   MOVW  0xA ; 100 * 100us = 10ms 
-   MOVWR PRE  ; turn on the prescaler
-   MOVW  0XA ; set the delay timer
-   MOVWR TMR  ; put delay amount into tmr
+   MOVW     0x64        ; move 100 into pre, 4MHz/100 = 40KHz new CK speed
+                        ; 100 * 100us = 10ms delay
+   MOVWR    PRE         ; turn on the prescaler
+   MOVW     0X64        ; move 100 into W to ...
+   MOVWR    TMR         ; set delay time for TMR
    ; since prescaler is running at 100us / clock
    ; 100 x ck speed will give a total of 10ms delay.
+   ; n * 10ms = delay (s)
 
 SRPOLL:
-   MOVRW SR
-   ANDW 0X02   ; check tmr flag in status register
-   JPZ SRPOLL  ; if TMR still low, keep polling for TMR high
-               ; when TMR goes high, don't loop but ...
-   DEC N_LENGTH ; decrement the note length register
-   JPZ ROTLOOP ; if loop through SRPOLL n times, rotate
-   JMP DELAY ; else goto delay
+   MOVRW    SR
+   ; run the loop until timer == 0
+   ; then decrement the note register so that the actual delay
+   ; is equal to TMR * note length
+   ; e.g., FOURd = 158 (9E) * 10ms = 1.58 seconds
+   ANDW     0X02        ; check TMR flag in status register
+   JPZ      SRPOLL      ; if TMR still low, keep polling for TMR high
+                        ; when TMR goes high, don't loop but ...
+   DEC      N_LENGTH    ; decrement the note length register
+   JPZ      ROTLOOP     ; if looped SRPOLL time/s (Z=1), goto stepper routine
+   JMP      DELAY       ; Z=0, therefore timer loop not over, go back to delay
+;=======================;
 
 ; == STEPPER CONTROL == ;
 ROTLOOP:
+    MOVW    0X00        ; turn off ...
+    MOVWR   PRE         ; ... the prescaler (PRE=0, TMR DISABLED)
     MOVW    ROTAMOUNT   ; rotate 14 times, T = 360/no. sectors(27) = 13.3^.
     MOVWR   R_COUNT     ; move rotamount into memory location
 REP:
-    MOVRW   R_COUNT     ; store delay value in memory location
-    SUBW    0X01        ; sub one from 14
-    MOVWR   R_COUNT     ; move new value back into memory location
+    DEC     R_COUNT     ; decrement the current rotation count
     JPZ     DONE        ; if delay == 0 then poll
     CALL    ROTATE      ; rotate once
     JMP     REP         ; repeat loop until 0xE0 == 0
@@ -93,27 +102,27 @@ DONE:
 ROTATE:
     MOVW    0X01        ; 1a
     MOVWR   PORTB       ; move to output
-    CALL    wait100ms
+    CALL    wait10ms    ; delay
     MOVW    0X02        ; 2a
     MOVWR   PORTB       ; move to output
-    CALL    wait100ms
+    CALL    wait10ms    ; delay
     MOVW    0X04        ; 1b
     MOVWR   PORTB       ; move to output
-    CALL    wait100ms
+    CALL    wait10ms    ; delay
     MOVW    0X08        ; 2b
     MOVWR   PORTB       ; move to output
-    CALL    wait100ms
+    CALL    wait10ms    ; delay
 RET
 ;=======================;
 
 ; ==== END OF FILE ==== ;
 EOF:
-   MOVW 0X01      	; move 1 into w
+   MOVW 0X01      	    ; move 1 into w
    MOVWR PORTB          ; turn on led
-   CALL wait100ms       ; keep it on for 1s
+   CALL wait100ms       ; keep it on for 0.1s
    MOVW 0X00            ; move 0 into w
    MOVWR PORTB          ; turn off led
-   CALL wait100ms       ; wait 1s
+   CALL wait100ms       ; wait 0.1s
    MOVRW PORTC          ; get portc input
    ANDW 0X03            ; can start again?
    JPZ EOF              ; if not, loop

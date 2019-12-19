7f33f08c1cdfaf37fffc5a96ac2b5e27ab240a66
Cass <m@cass.si>
2019-12-19 10:53:35 +0000

    mobile optimisations

--- layouts/Root.js
+++ layouts/Root.js
@@ -10,6 +10,7 @@ export default function Root(props) {
 				<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css" />
 				<link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
 				<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+	      <link rel="icon" type="image/png" href="/static/daughtersystems.png" />
 			</Head>
 
 			{ props.children }
